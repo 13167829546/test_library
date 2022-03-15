a932cbe463dcc42d9de39b7730fa3014510ebe52
test <a954739979@163.com>
2022-03-15 14:55:19 +0800

    2

--- 1.自动化执行项目/testcase/runTestAll.py
+++ 1.自动化执行项目/testcase/runTestAll.py
@@ -5,6 +5,9 @@ import sys
 import os
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
+#jenkins自动化构建用一下
+curPath = 'C:\\Users\\huwo\\.jenkins\\workspace\\测试\\1.自动化执行项目\\testcase'
+rootPath = 'C:\\Users\\huwo\\.jenkins\\workspace\\测试\\1.自动化执行项目'
 sys.path.append(rootPath)
 from common.FilePath import FilePath
 from testcase.Login_C import Login_C
