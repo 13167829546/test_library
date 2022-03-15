b9a420413f2a904effdde3f61c853930a01aacd5
test <a954739979@163.com>
2022-03-15 14:33:28 +0800

    1

--- 1.自动化执行项目/common/FilePath.py
+++ 1.自动化执行项目/common/FilePath.py
@@ -1,9 +1,9 @@
 class FilePath():
     def get_File_Path(self,flag):
-        filePath = {'fileDataPath':'C:\\Users\\huwo\\Desktop\\1.自动化执行项目\\document\\牵引作业系统自动化.xls',\
-                    'logPath':'C:\\Users\\huwo\\Desktop\\1.自动化执行项目\\logs',\
-                    'reportPath':'C:\\Users\\huwo\\Desktop\\1.自动化执行项目\\report\\result.html',\
-                    'testfile':'C:\\Users\\huwo\\Desktop\\1.自动化执行项目\\document\\'}
+        filePath = {'fileDataPath':'C:\\Users\\huwo\\.jenkins\\workspace\\测试\\1.自动化执行项目\\document\\牵引作业系统自动化.xls',\
+                    'logPath':'C:\\Users\\huwo\\.jenkins\\workspace\\测试\\1.自动化执行项目\\logs',\
+                    'reportPath':'C:\\Users\\huwo\\.jenkins\\workspace\\测试\\1.自动化执行项目\\report\\result.html',\
+                    'testfile':'C:\\Users\\huwo\\.jenkins\\workspace\\测试\\1.自动化执行项目\\document\\'}
         for pathkey in filePath.keys():
             if flag == pathkey:
                 #print(filePath[pathkey])
